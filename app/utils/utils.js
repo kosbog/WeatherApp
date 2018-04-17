22dc3f179540f025bf2a781fc12b65f978602732
kosbog <badykos@gmail.com>
2018-04-17 20:09:01 +0300

    fix

--- app/utils/utils.js
+++ app/utils/utils.js
@@ -11,19 +11,6 @@ export const reloadPage = () => {
 }
 
 export const parseDate = (elem) => {
-<<<<<<< HEAD
-    let tempDay = new Date(elem).toDateString(),
-        dateArr = tempDay.split(' '),
-        firstNumber = dateArr[2][0],
-        secondNumber = dateArr[2][1],
-        date;
-
-    date = firstNumber === '0' ? secondNumber : dateArr[2];
-
-    return {
-        name: dateArr[0],
-        month: dateArr[1],
-=======
     const weekendArr = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     const monthArr = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
@@ -35,7 +22,6 @@ export const parseDate = (elem) => {
     return {
         day: day,
         month: month,
->>>>>>> e28ff84c6f0cb74a89740313cdf88b6b637e70e6
         date: date
     };
 }
